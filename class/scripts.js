c7a40f21673aea336b262a60fe89440318ce3614
Gabby Carnabuci <carng328@newschool.edu>
2016-09-16 22:15:31 +0000

    up

---
+++ class/scripts.js
@@ -0,0 +1,75 @@
+console.log("hello world");
+
+var timer = 0;
+var counter = 0;
+var numbers = [];
+
+for (var x = 0; x < 100; x++) {
+    numbers.push(x)
+} 
+
+
+console.log(snow);
+
+for (var i = 0; i < 100; i++){
+    setTimeout(function(){
+        var snow = "";
+        for (var s = 0; s < 40; s++;
+                if (Math.random() *100 < 70){
+                snow += "*";
+            }   else {
+                snow += " ";
+        }
+        console.log("**  **  **  **  **")
+    }, timer);
+    timer += 100;
+    counter++;
+}
+
+// setTimeout(function() {
+//     console.log("you made it");
+// }, (Math.random()*5000));
+
+
+// setTimeout(function() {
+//     console.log("you made it");
+// }, 3000);
+
+// var user = "Gabby";
+// var password = "hello";
+// var i = 0;
+
+// var lastnum = false;
+
+// for (i = 0; i < 1000; i++) {
+//     var newnum = Math.floor(Math.random()*10);
+//     while (lastnum == newnum) {
+//         // console.log("Duplicate detected");
+//         newnum = Math.floor(Math.random()*10);
+//     }
+//     console.log(newnum);
+//     lastnum = newnum;
+// }
+
+
+// var i = 0;
+// while (i < 1000) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// for (var i=0; i < 10; i++){
+//     console.log(i * 6);
+// }
+
+
+// if (user == "Gabby" && password == "letmein") {
+//     console.log("you made it...")
+// } else {
+//     if (user == "Gabby"){
+//         console.log("Sorry, wrong password");
+//     } else {
+//         console.log("Sorry, wrong username");
+//     }
+// }
