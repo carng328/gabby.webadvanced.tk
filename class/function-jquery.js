b0fa2f8c7659a3af4a6937072ba9384a1988429e
Gabby Carnabuci <carng328@newschool.edu>
2016-10-27 03:11:08 +0000

    uploaded

---
+++ class/function-jquery.js
@@ -0,0 +1,12 @@
+function sayHello() {
+    console.log("hello hello");
+
+}
+
+function addNums(num1, num2) {
+    console.log(num1 + num2);
+}
+
+var mySym = addNums(10,20);
+
+var fruits = ["Apple", "Orange", "Carrot" ]
